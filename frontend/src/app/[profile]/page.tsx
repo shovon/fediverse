2ccdea02d205cb3b8d2c000d3d3649e208805ecd
Sal Rahman <salehen.rahman@gmail.com>
2023-07-30 10:44:10 -0700

    No need of the frontend

--- frontend/src/app/[profile]/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Profile() {
-	return <div>This is supposed to be a profile page</div>;
-}
--- frontend/src/app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-	return <main>Interesting</main>;
-}

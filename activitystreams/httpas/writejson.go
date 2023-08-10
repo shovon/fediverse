46d858fa1cc2711b21a0437ef7edc99bde5ed51a
Sal Rahman <salehen.rahman@gmail.com>
2023-08-09 22:57:00 -0700

    More work

--- activitystreams/httpas/writejson.go
+++ activitystreams/httpas/writejson.go
@@ -6,6 +6,6 @@ import (
 )
 
 func WriteJSON(w http.ResponseWriter, r *http.Request, v interface{}) error {
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json+activity; charset=utf-8")
 	return json.NewEncoder(w).Encode(v)
 }

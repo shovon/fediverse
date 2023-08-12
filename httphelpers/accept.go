1ef634baa55f0c08c69d8bc37a97cf24b8db6a32
Sal Rahman <salehen.rahman@gmail.com>
2023-08-12 00:18:16 -0700

    More work done

--- httphelpers/accept.go
+++ httphelpers/accept.go
@@ -10,30 +10,28 @@ import (
 
 type negotiatedMediaType struct{}
 
-func Accept(supported ...string) func(func(http.Handler) http.Handler) func(http.Handler) http.Handler {
-	return func(middleware func(http.Handler) http.Handler) func(http.Handler) http.Handler {
-		return func(next http.Handler) http.Handler {
-			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				negotiator := contentnegotiation.NewNegotiator(supported...)
-				negotiated, provided, err := negotiator.Negotiate(r.Header.Get("Accept"))
-				if err != nil {
-					next.ServeHTTP(w, r)
-					return
-				}
+func Accept(supported []string, middleware func(http.Handler) http.Handler) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			negotiator := contentnegotiation.NewNegotiator(supported...)
+			negotiated, provided, err := negotiator.Negotiate(r.Header.Get("Accept"))
+			if err != nil {
+				next.ServeHTTP(w, r)
+				return
+			}
 
-				middleware(next).
-					ServeHTTP(
-						w,
-						r.WithContext(
-							context.WithValue(
-								r.Context(),
-								negotiatedMediaType{},
-								NegotiatedMediaType{negotiated, provided},
-							),
+			middleware(next).
+				ServeHTTP(
+					w,
+					r.WithContext(
+						context.WithValue(
+							r.Context(),
+							negotiatedMediaType{},
+							NegotiatedMediaType{negotiated, provided},
 						),
-					)
-			})
-		}
+					),
+				)
+		})
 	}
 }
 

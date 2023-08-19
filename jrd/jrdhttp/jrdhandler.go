1ea0e317fbc2adfa63a7aba2f2f4daf7d2b014b0
Sal Rahman <salehen.rahman@gmail.com>
2023-08-18 23:44:59 -0700

    Fix to a bug

--- jrd/jrdhttp/jrdhandler.go
+++ jrd/jrdhttp/jrdhandler.go
@@ -1,23 +1,14 @@
 package jrdhttp
 
 import (
-	"fediverse/httphelpers"
 	"fediverse/httphelpers/httperrors"
 	"fediverse/jrd"
+	"fediverse/json/jsonhttp"
 	"net/http"
 )
 
 func CreateJRDHandler(handler func(r *http.Request) (jrd.JRD, httperrors.HTTPError)) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		j, errjrd := handler(r)
-		if errjrd != nil {
-			errjrd.ServeHTTP(w, r)
-			return
-		}
-		err := httphelpers.WriteJSON(w, j)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("Internal server error"))
-		}
+	return jsonhttp.JSONResponder(func(r *http.Request) (any, error) {
+		return handler(r)
 	})
 }

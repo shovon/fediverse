e52de07ca786f51c55abd85396bf7acacc756122
Sal Rahman <salehen.rahman@gmail.com>
2023-08-23 22:28:01 -0700

    More work

---
+++ httphelpers/authentication/parseauth.go
@@ -0,0 +1,25 @@
+package authentication
+
+import (
+	"regexp"
+	"strings"
+	"unicode/utf8"
+)
+
+type Authorization struct {
+	Scheme string
+	Params string
+}
+
+func ParseAuthorization(authorization string) Authorization {
+	pattern := regexp.MustCompile("[ \t]+")
+	parts := pattern.Split(authorization, -1)
+	if len(parts) < 1 {
+		return Authorization{}
+	}
+	scheme := strings.ToLower(parts[0])
+
+	params := string(([]rune(authorization))[:utf8.RuneCountInString(scheme)+1])
+
+	return Authorization{Scheme: scheme, Params: params}
+}

68fdc039b252a2c6bb420ec0b4e968523fca5ab0
Sal Rahman <salehen.rahman@gmail.com>
2023-08-24 15:56:31 -0700

    Digestion

---
+++ httphelpers/rfc3230/adddigeststoheaders.go
@@ -0,0 +1,32 @@
+package rfc3230
+
+import (
+	"fediverse/pair"
+	"net/http"
+)
+
+type Digester interface {
+	Token() string
+	Digest([]byte) (string, error)
+}
+
+func AddDigestsToheaders(h *http.Header, body []byte, digesters []Digester) error {
+	digestParts := []pair.Pair[string, string]{}
+
+	for _, digester := range digesters {
+		digest, err := digester.Digest(body)
+		if err != nil {
+			return err
+		}
+		digestParts = append(
+			digestParts,
+			pair.Pair[string, string]{Key: digester.Token(), Value: digest},
+		)
+	}
+
+	digest := CreateDigestString(digestParts)
+
+	h.Add("Digest", digest)
+
+	return nil
+}

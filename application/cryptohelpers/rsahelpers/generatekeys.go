1300bd513d66b98038aae04ced411739ac024001
Sal Rahman <salehen.rahman@gmail.com>
2023-08-29 22:39:32 -0700

    More refactoring

--- application/cryptohelpers/rsahelpers/generatekeys.go
+++ application/cryptohelpers/rsahelpers/generatekeys.go
@@ -7,6 +7,11 @@ import (
 	"encoding/pem"
 )
 
+const (
+	RSAPrivateKeyLabel = "RSA PRIVATE KEY"
+	PublicKeyLabel     = "PUBLIC KEY"
+)
+
 func GenerateRSPrivateKey(bits int) (*rsa.PrivateKey, error) {
 	privateKey, err := rsa.GenerateKey(rand.Reader, bits)
 	if err != nil {
@@ -17,7 +22,7 @@ func GenerateRSPrivateKey(bits int) (*rsa.PrivateKey, error) {
 
 func PrivateKeyToPKCS1PEMBlock(key *rsa.PrivateKey) *pem.Block {
 	return &pem.Block{
-		Type:  "RSA PRIVATE KEY",
+		Type:  RSAPrivateKeyLabel,
 		Bytes: x509.MarshalPKCS1PrivateKey(key),
 	}
 }
@@ -32,7 +37,7 @@ func PublicKeyToPKIXBlock(key *rsa.PublicKey) (*pem.Block, error) {
 		return nil, err
 	}
 	return &pem.Block{
-		Type:  "PUBLIC KEY",
+		Type:  PublicKeyLabel,
 		Bytes: publicKeyPEMBytes,
 	}, nil
 }
@@ -44,3 +49,30 @@ func PublicKeyToPKIXString(key *rsa.PublicKey) (string, error) {
 	}
 	return string(pem.EncodeToMemory(publicKeyPEMBlock)), nil
 }
+
+// So this library does not provide a way to parse neither the PKCS1 PEM private
+// key, nor the PKIX PEM public key. This is because parsing has to places where
+// it can fail: the PEM decoding, and the PKCS1 or PKIX parsing. Probably a good
+// idea to force the client code to handle that.
+//
+// To parse the private key:
+//
+//     block, _ := pem.Decode([]byte(content))
+//     if block == nil || block.Type != "RSA PRIVATE KEY" {
+//         // Handle error
+//     }
+//     privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+//     if err != nil {
+//       // Handle error
+//     }
+//
+// To parse the public key:
+//
+//     block, _ := pem.Decode([]byte(content))
+//     if block == nil || block.Type != "PUBLIC KEY" {
+//         // Handle error
+//     }
+//     publicKey, err := x509.ParsePKIXPublicKey(block.Bytes)
+//     if err != nil {
+//       // Handle error
+//     }

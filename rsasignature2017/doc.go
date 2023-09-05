9b45b1a06daab9d29084b544d3e15f8b9bd23d6b
Sal Rahman <salehen.rahman@gmail.com>
2023-09-04 23:23:40 -0700

    Some documentation

--- rsasignature2017/doc.go
+++ rsasignature2017/doc.go
@@ -4,4 +4,15 @@ package rsasignature2017
 //
 // Apparently, RsaSignature2017 is needed for something. Unclear what.
 //
+// Here's the article that details how to work with RsaSignature2017:
+// https://socialhub.activitypub.rocks/t/making-sense-of-rsasignature2017/347
+//
+// I first heard about it here:
+// https://techhub.social/@justin@ser.endipito.us/111010669352202814
+// (https://ser.endipito.us/@justin/111010669345241649)
+//
+// Also, someone is mentioning that LD-Signatures are a legacy thing, and
+// instead recommending to work with this:
+// https://codeberg.org/fediverse/fep/src/branch/main/fep/8b32/fep-8b32.md
+//
 // Creating this empty module to remind myself to implement it later.

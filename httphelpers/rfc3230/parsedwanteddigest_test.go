110045e823bf69d848a87e6417f9d27d20adad28
Sal Rahman <salehen.rahman@gmail.com>
2023-09-10 00:25:24 -0700

    More testing

--- httphelpers/rfc3230/parsedwanteddigest_test.go
+++ httphelpers/rfc3230/parsedwanteddigest_test.go
@@ -75,25 +75,81 @@ func TestParseWantedDigest(t *testing.T) {
 			}
 
 			if !digests[0].Right.Equal(decimal.NewFromInt(1)) {
-				t.Errorf("expected q value %s, got %s", "0.5", digests[0].Right.String())
+				t.Errorf("expected q value %s, got %s", "1", digests[0].Right.String())
 			}
 
 			if !digests[1].Right.Equal(decimal.NewFromInt(1)) {
-				t.Errorf("expected q value %s, got %s", "0.5", digests[1].Right.String())
+				t.Errorf("expected q value %s, got %s", "1", digests[1].Right.String())
 			}
 		})
 
 		t.Run("multiple with q", func(t *testing.T) {
-			_, err := ParseWantedDigest("sha-256;q=0.5,sha-512;q=0.5")
+			digests, err := ParseWantedDigest("sha-256;q=0.5,sha-512;q=0.5")
 			if err != nil {
 				t.Error("unexpected error")
+			}
+
+			if len(digests) != 2 {
+				t.Error("expected 2 digests")
+			}
+
+			if digests[0].Left != "sha-256" {
+				t.Errorf("expected digest %s, got %s", "sha-256", digests[0].Left)
+			}
+
+			if digests[1].Left != "sha-512" {
+				t.Errorf("expected digest %s, got %s", "sha-512", digests[0].Left)
+			}
+
+			digest1, err := decimal.NewFromString("0.5")
+			if err != nil {
+				panic(err)
+			}
+			if !digests[0].Right.Equal(digest1) {
+				t.Errorf("expected q value %s, got %s", "0.5", digests[0].Right.String())
+			}
+
+			digest2, err := decimal.NewFromString("0.5")
+			if err != nil {
+				panic(err)
+			}
+			if !digests[1].Right.Equal(digest2) {
+				t.Errorf("expected q value %s, got %s", "0.5", digests[1].Right.String())
 			}
 		})
 
 		t.Run("multiple with q and spaces", func(t *testing.T) {
-			_, err := ParseWantedDigest("sha-256;q=0.5, sha-512;q=0.5")
+			digests, err := ParseWantedDigest("sha-256;q=0.5, sha-512;q=0.5")
 			if err != nil {
 				t.Error("unexpected error")
+			}
+
+			if len(digests) != 2 {
+				t.Error("expected 2 digests")
+			}
+
+			if digests[0].Left != "sha-256" {
+				t.Errorf("expected digest %s, got %s", "sha-256", digests[0].Left)
+			}
+
+			if digests[1].Left != "sha-512" {
+				t.Errorf("expected digest %s, got %s", "sha-512", digests[0].Left)
+			}
+
+			digest1, err := decimal.NewFromString("0.5")
+			if err != nil {
+				panic(err)
+			}
+			if !digests[0].Right.Equal(digest1) {
+				t.Errorf("expected q value %s, got %s", "0.5", digests[0].Right.String())
+			}
+
+			digest2, err := decimal.NewFromString("0.5")
+			if err != nil {
+				panic(err)
+			}
+			if !digests[1].Right.Equal(digest2) {
+				t.Errorf("expected q value %s, got %s", "0.5", digests[1].Right.String())
 			}
 		})
 

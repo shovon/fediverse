e687dd32c16f309b4defe01527ee96dd6933cf24
Sal Rahman <salehen.rahman@gmail.com>
2023-09-23 22:31:50 -0700

    More work done

--- fedi/actor.go
+++ fedi/actor.go
@@ -1,8 +1,11 @@
 package fedi
 
 import (
+	"errors"
 	"fediverse/acct"
 	"fediverse/webfinger"
+	"io"
+	"net/http"
 )
 
 type Account struct {
@@ -14,7 +17,30 @@ func GetActor(account Account) (any, error) {
 	// First, we issue a WebFinger request to the specific host, requesting
 	// specifically for the application/activity+json content type.
 
-	// But before that, construct a WebFinger URL.
+	j, err := webfinger.Lookup(account.Host, acct.Acct(account).String(), []string{"self"})
+	if err != nil {
+		return nil, err
+	}
 
-	return webfinger.Lookup(account.Host, acct.Acct(account).String(), []string{"self"})
+	links, ok := j.Links.Value()
+	if !ok {
+		return nil, errors.New("no links found")
+	}
+
+	for _, link := range links {
+		if link.Rel == "self" {
+			req, err := http.NewRequest("GET", link.Href, nil)
+			if err != nil {
+				return nil, err
+			}
+			fullLD, err := io.ReadAll(req.Body)
+			if err != nil {
+				return nil, err
+			}
+			return fullLD, nil
+			break
+		}
+	}
+
+	return nil, errors.New("no self links found")
 }
